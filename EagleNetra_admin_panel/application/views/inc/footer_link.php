4c15cda82e850158a1a8b22a8abd36cb5fe6533d
skrohan0420 <skrohan0420@gmail.com>
2022-12-30 18:50:04 +0530

    --EagleNetra_admin_inc_folder  --

--- EagleNetra_admin_panel/application/views/inc/footer_link.php
+++ EagleNetra_admin_panel/application/views/inc/footer_link.php
@@ -1,16 +1,11 @@
  <!-- END PAGA BACKDROPS-->
     <!-- CORE PLUGINS -->
-    <!-- <script src="./assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script> -->
     <script src="<?= base_url('./assets/vendors/jquery/dist/jquery.min.js')?>" type="text/javascript"></script>
-    <!-- <script src="./assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script> -->
     <script src="<?= base_url('./assets/vendors/popper.js/dist/umd/popper.min.js')?>" type="text/javascript"></script>
-    <!-- <script src="./assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script> -->
     <script src="<?= base_url('./assets/vendors/bootstrap/dist/js/bootstrap.min.js')?>" type="text/javascript"></script>
     <!-- PAGE LEVEL PLUGINS -->
-    <!-- <script src="./assets/vendors/jquery-validation/dist/jquery.validate.min.js" type="text/javascript"></script> -->
     <script src="<?= base_url('./assets/vendors/jquery-validation/dist/jquery.validate.min.js')?>" type="text/javascript"></script>
     <!-- CORE SCRIPTS-->
-    <!-- <script src="assets/js/app.js" type="text/javascript"></script> -->
     <script src="<?= base_url('assets/js/app.js')?>" type="text/javascript"></script>
     <!-- PAGE LEVEL SCRIPTS-->
     <script type="text/javascript">
